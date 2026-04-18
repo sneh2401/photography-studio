a3af858eb82c801d0528d4b90635ed02cd928fbe
sneh2401 <sbehpatel121@gmail.com>
2026-04-18 16:03:13 +0530

    updated api url for production

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://photography-studio-backend.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
